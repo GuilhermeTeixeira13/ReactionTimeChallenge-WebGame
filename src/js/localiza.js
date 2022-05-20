16c07bd9e96ebb6b9cf3a78e8256e80ebec1edb2
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2022-05-20 17:04:56 +0100

    Update localiza.js

--- src/js/localiza.js
+++ src/js/localiza.js
@@ -26,6 +26,8 @@ function displayLocation(latitude, longitude) {
           state = value[count - 2];
           city = value[count - 3];
           const Cidade = state.split(" ");
+          console.log("sadasd");
+          console.log(" sad");
           console.log("---" + Cidade[2]);
           var cid = Cidade[2];
           console.log("---" + cid);
