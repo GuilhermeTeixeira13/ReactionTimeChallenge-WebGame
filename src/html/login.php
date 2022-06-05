77f75e98ad7ed8316521e974a8f199fe4cee5f4a
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-06-05 14:49:06 +0100

    Update login.php

--- src/html/login.php
+++ src/html/login.php
@@ -2,12 +2,7 @@
 session_start();
 include "db_conn.php";
 
-
-
 if(isset($_POST['uname']) && isset($_POST['password'])) {
-
-
-
     function validate($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -15,24 +10,23 @@ if(isset($_POST['uname']) && isset($_POST['password'])) {
         return $data;
     }
 
-
     $uname = validate($_POST['uname']);
     $password = validate($_POST['password']);
 
-    $sql = "SELECT * FROM users WHERE username='$uname' AND userpassword='$password'";
+    $sql = "SELECT * FROM users WHERE Username='$uname' AND Userpassword='$password'";
 
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result) === 1) {
         $row = mysqli_fetch_assoc($result);
-        if($row['username'] === $uname && $row['userpassword'] === $password) {
+        if($row['Username'] === $uname && $row['Userpassword'] === $password) {
             $_SESSION['id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             header("Location: game.php");
             exit();
         }
         else {
-            header("Location: game.php?error=Incorret Username or PasswordCHN");
+            header("Location: game.php?error=Incorret Username or Password");
             exit();
         }
     }else {
